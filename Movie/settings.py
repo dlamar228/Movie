c9aee7c29c5b18a2f5242db34c19995014cb0967
dlamar <dlamar-dima@mail.ru>
2021-02-09 16:18:51 +0300

    Fix header bug

--- Movie/settings.py
+++ Movie/settings.py
@@ -141,8 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR,"static")
+
 STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = "/media/"
