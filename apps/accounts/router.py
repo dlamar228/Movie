631d4c98aaf4da213a26cec690dd7e53e524bea0
dlamar <dlamar-dima@mail.ru>
2021-02-13 00:48:47 +0300

    Fix db v3 !

--- apps/accounts/router.py
+++ apps/accounts/router.py
@@ -13,15 +13,17 @@ class UsersRouter(object):
     
     def allow_relation(self, obj1, obj2, **hints):
         "Allow any relation if a both models in chinook app"
-        if obj1._meta.app_label == 'auth' and obj2._meta.app_label == 'auth':
+
+        '''if obj1._meta.app_label == 'auth' and obj2._meta.app_label == 'auth':
             return True
         # Allow if neither is chinook app
         elif 'auth' not in [obj1._meta.app_label, obj2._meta.app_label]: 
-            return True
-        return False
+            return True'''
+        return True
     
     def allow_syncdb(self, db, model):
-        if db == 'users' or model._meta.app_label == "auth":
+        '''if db == 'users' or model._meta.app_label == "auth":
             return False # we're not using syncdb on our legacy database
         else: # but all other models/databases are fine
-            return True
+            return True'''
+        return True
